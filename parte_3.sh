10d26715fee4c5245bb8fc67c8be2a677855803f
Carmen Gabriela Arocha Valero <carocha@fi.uba.ar>
2025-09-09 18:31:22 +0000

    Agrego en el bloque if un

--- parte_3.sh
+++ parte_3.sh
@@ -11,9 +11,9 @@ fi
 mkdir -p "$directorio_entrada"
 ./parte_1.sh "$padron_entrada" "$directorio_entrada"
 
-if [ ! -f "$directorio_entrada"/resultado.txt" ]
+if [ ! -f "$directorio_entrada"/resultado.txt" ]; then
    echo "No se encontró $directorio_entrada/resultado.txt"
    exit 1
 fi
 
-./parte_2.sh < "$directorio_resultado"/resultado.txt > "$directorio_entrada/output.txt"
+./parte_2.sh < "$directorio_entrada"/resultado.txt > "$directorio_entrada/output.txt"
