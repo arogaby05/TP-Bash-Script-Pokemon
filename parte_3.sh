0ecad4cdc5b0f14dcb27d0d9450cfd6b7677e2e6
Carmen Gabriela Arocha Valero <carocha@fi.uba.ar>
2025-09-09 18:11:31 +0000

    Agrego 2 verificaciones: padron y resultado.txt

--- parte_3.sh
+++ parte_3.sh
@@ -3,7 +3,18 @@
 padron_entrada=$1
 directorio_entrada="./resultado"
 
+if [ -z "$padron_entrada" ]; then
+   echo "Usar $0 con <PADRON>"
+   exit 1
+fi
+
 mkdir -p "$directorio_entrada"
 ./parte_1.sh "$padron_entrada" "$directorio_entrada"
 
-./parte_2.sh "$directorio_resultado"/resultado.txt > "$directorio_entrada"/output.txt
+if [ ! -f "$directorio_entrada"/resultado.txt" ]
+   echo "No se encontró $directorio_entrada/resultado.txt"
+   exit 1
+fi
+
+./parte_2.sh < "$directorio_resultado"/resultado.txt > "$directorio_entrada/output.txt"
+echo "Proceso terminado. Los resultados se encuentran en $directorio_entrada/output.txt"
